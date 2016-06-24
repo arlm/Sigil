419340d7d07410af9aae568de5a654ef19505166
Константин Дьяченко <konard@me.com>
2016-06-24 18:46:17 +0300

    "dnx test" is not working anymore, replaced with "dotnet test".

--- SigilTests.DNX/Program.cs
+++ SigilTests.DNX/Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main()
         {
-            System.Console.WriteLine("please use: dnx test");
+            System.Console.WriteLine("please use: dotnet test");
         }
     }
 }
